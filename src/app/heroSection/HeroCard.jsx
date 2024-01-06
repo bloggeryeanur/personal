c2cb8d80eaad032b1d738ebe0b167a8ecf3fff49
bloggeryeanur <bloggeryeanur@gmail.com>
2024-01-07 04:17:55 +0600

    fixed cv download button

--- src/app/heroSection/HeroCard.jsx
+++ src/app/heroSection/HeroCard.jsx
@@ -2,6 +2,29 @@ import { Box, Button, Typography } from "@mui/material";
 import Image from "next/image";
 
 export default function HeroCard({ img, title, desc1, desc2 }) {
+
+  // Resume Download button
+  const handleDownload = () => {
+    // Replace 'sample.pdf' with the actual path to your PDF file
+    const pdfPath = '../BSc_Certificate.pdf';
+    downloadPDF(pdfPath);
+  };
+
+  const downloadPDF = (path) => {
+    fetch(path)
+      .then((response) => response.blob())
+      .then((blob) => {
+        const url = window.URL.createObjectURL(new Blob([blob]));
+        const link = document.createElement('a');
+        link.href = url;
+        link.setAttribute('download', 'downloaded.pdf');
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+      })
+      .catch((error) => console.error('Error downloading PDF:', error));
+  };
+
   return (
     <Box style={{ position: "relative", textAlign: "center" }}>
       <Image
@@ -21,7 +44,7 @@ export default function HeroCard({ img, title, desc1, desc2 }) {
           <Typography variant="h4">{title}</Typography>
           <Typography>{desc1}</Typography>
           <Typography>{desc2}</Typography>
-          <Button variant="contained" color="secondary" sx={{ mt: 2 }}>
+          <Button variant="contained" color="secondary" sx={{ mt: 2 }} onClick={handleDownload}>
             Download CV
           </Button>
         </Box>
